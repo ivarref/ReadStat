a9b98a1bbbf0bd5ce50b489cf6f0d28be2140135
Ivar Refsdal <ivar.refsdal@nsd.no>
2016-10-31 09:27:21 +0100

    Small fixes extract metadata

--- src/bin/extract_metadata.c
+++ src/bin/extract_metadata.c
@@ -15,25 +15,24 @@ typedef struct context {
     readstat_label_set_t *label_set;
 } context;
 
-readstat_label_set_t * get_label_set(const char *val_labels, struct context *ctx, int* is_new) {
-    if (is_new) {
-        *is_new = 0;
-    }
+readstat_label_set_t * get_label_set(const char *val_labels, struct context *ctx, int alloc_new) {
     for (int i=0; i<ctx->variable_count; i++) {
         readstat_label_set_t * lbl = &ctx->label_set[i];
         if (0 == strcmp(lbl->name, val_labels)) {
             return lbl;
         }
     }
+    if (!alloc_new) {
+        fprintf(stderr, "%s:%d could not find value labels %s\n", __FILE__, __LINE__, val_labels);
+        return NULL;
+    }
+
     ctx->label_set = realloc(ctx->label_set, ++ctx->variable_count*sizeof(readstat_label_set_t));
     if (!ctx->label_set) {
         fprintf(stderr, "%s:%d realloc error: %s\n", __FILE__, __LINE__, strerror(errno));
         return NULL;
     }
     readstat_label_set_t * lbl = &ctx->label_set[ctx->variable_count-1];
-    if (is_new) {
-        *is_new = 1;
-    }
     memset(lbl, 0, sizeof(readstat_label_set_t));
     return lbl;
 }
@@ -41,13 +40,17 @@ readstat_label_set_t * get_label_set(const char *val_labels, struct context *ctx
 static int handle_value_label(const char *val_labels, readstat_value_t value, const char *label, void *c) {
     struct context *ctx = (struct context*)c;
     if (value.type == READSTAT_TYPE_DOUBLE && strncmp(val_labels, "labels", strlen("labels")) == 0) {
-        readstat_label_set_t * label_set = get_label_set(val_labels, ctx, NULL);
+        readstat_label_set_t * label_set = get_label_set(val_labels, ctx, 1);
         if (!label_set) {
             return READSTAT_ERROR_MALLOC;
         }
         snprintf(label_set->name, sizeof(label_set->name)-1, "%s", val_labels);
         long label_idx = label_set->value_labels_count;
         label_set->value_labels = realloc(label_set->value_labels, (1 + label_idx) * sizeof(readstat_value_label_t));
+        if (!label_set->value_labels) {
+            fprintf(stderr, "%s:%d realloc error: %s\n", __FILE__, __LINE__, strerror(errno));
+            return READSTAT_ERROR_MALLOC;
+        }
         readstat_value_label_t* value_label = &label_set->value_labels[label_idx];
         memset(value_label, 0, sizeof(readstat_value_label_t));
         value_label->double_key = value.v.double_value;
@@ -60,8 +63,6 @@ static int handle_value_label(const char *val_labels, readstat_value_t value, co
 
 int handle_variable (int index, readstat_variable_t *variable, const char *val_labels, void *my_ctx) {
     struct context *ctx = (struct context *)my_ctx;
-    int new = 0;
-    int* newp = &new;
 
     char* type = "";
     if (variable->type == READSTAT_TYPE_STRING) {
@@ -100,11 +101,10 @@ int handle_variable (int index, readstat_variable_t *variable, const char *val_l
         fprintf(ctx->fp, ", \"label\": \"%s\" ", variable->label);
     }
 
-    printf("variable index is %d for %s\n", variable->index, variable->name);
     if (val_labels) {
-        readstat_label_set_t * label_set = get_label_set(val_labels, ctx, newp);
-        if (*newp) {
-            fprintf(stderr, "Did not expect to allocate new label set!\n");
+        readstat_label_set_t * label_set = get_label_set(val_labels, ctx, 0);
+        if (!label_set) {
+            fprintf(stderr, "Could not find label set %s!\n", val_labels);
             exit(EXIT_FAILURE);
         }
         fprintf(ctx->fp, ", \"categories\": [");
