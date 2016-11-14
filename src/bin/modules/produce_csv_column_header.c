b16294ddd0002508b015de17262f4ef521edca59
Ivar Refsdal <ivar.refsdal@nsd.no>
2016-11-14 13:19:08 +0100

    Support high and low in SPSS missing header

--- src/bin/modules/produce_csv_column_header.c
+++ src/bin/modules/produce_csv_column_header.c
@@ -121,7 +121,7 @@ char* produce_value_label(char* column, size_t len, struct csv_metadata *c, read
     return column;
 }
 
-double get_double_date_missing_sav(const char *js, jsmntok_t* missing_value_token, int idx) {
+double get_double_date_missing_sav(const char *js, jsmntok_t* missing_value_token) {
     // SAV missing date
     char buf[255];
     char *dest;
@@ -161,7 +161,7 @@ readstat_value_t get_int32_date_missing_dta(const char *js, jsmntok_t* missing_v
     return value;
 }
 
-double get_double_missing(const char *js, jsmntok_t* missing_value_token, int idx) {
+double get_double_missing(const char *js, jsmntok_t* missing_value_token) {
     char buf[255];
     char *dest;
     int len = missing_value_token->end - missing_value_token->start;
@@ -174,10 +174,76 @@ double get_double_missing(const char *js, jsmntok_t* missing_value_token, int id
     return val;
 }
 
-void produce_missingness(struct csv_metadata *c, const char* column) {
+void produce_missingness_discrete(struct csv_metadata *c, jsmntok_t* missing, const char* column) {
     readstat_variable_t* var = &c->variables[c->columns];
     int is_date = c->is_date[c->columns];
     const char *js = c->json_md->js;
+
+    jsmntok_t* values = find_object_property(js, missing, "values");
+    if (!values) {
+        fprintf(stderr, "%s:%d Expected to find missing 'values' property\n", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    int j = 1;
+    for (int i=0; i<values->size; i++) {
+        jsmntok_t* missing_value_token = values + j;
+        if (c->output_format == RS_FORMAT_SAV && var->type == READSTAT_TYPE_DOUBLE && is_date) {
+            readstat_variable_add_missing_double_value(var, get_double_date_missing_sav(js, missing_value_token));
+        } else if (c->output_format == RS_FORMAT_DTA && var->type == READSTAT_TYPE_INT32 && is_date) { 
+            // TODO I think this is correct, but would be good to verify
+            // So it seems that STATA does not use the hi-low ranges, and thus we only need to write once
+            readstat_value_t v = get_int32_date_missing_dta(js, missing_value_token, i);
+            var->missingness.missing_ranges[var->missingness.missing_ranges_count++] = v;
+        } else if (var->type == READSTAT_TYPE_DOUBLE) {
+            readstat_variable_add_missing_double_value(var, get_double_missing(js, missing_value_token));
+        } else {
+            fprintf(stderr, "%s:%d Unsupported column type %d\n", __FILE__, __LINE__, var->type);
+            exit(EXIT_FAILURE);
+        }
+        j += slurp_object(missing_value_token);
+    }
+}
+
+void produce_missingness_range(struct csv_metadata *c, jsmntok_t* missing, const char* column) {
+    readstat_variable_t* var = &c->variables[c->columns];
+    int is_date = c->is_date[c->columns];
+    const char *js = c->json_md->js;
+
+    jsmntok_t* low = find_object_property(js, missing, "low");
+    jsmntok_t* high = find_object_property(js, missing, "high");
+    jsmntok_t* discrete = find_object_property(js, missing, "discrete-value");
+
+    if (low && !high) {
+        fprintf(stderr, "%s:%d missing.low specified for column %s, but missing.high not specified\n", __FILE__, __LINE__, column);
+        exit(EXIT_FAILURE);
+    }
+    if (high && !low) {
+        fprintf(stderr, "%s:%d missing.high specified for column %s, but missing.low not specified\n", __FILE__, __LINE__, column);
+        exit(EXIT_FAILURE);
+    }
+
+    if (low && high && c->output_format == RS_FORMAT_SAV) {
+        double lo = is_date ? get_double_date_missing_sav(js, low) : get_double_missing(js, low);
+        double hi = is_date ? get_double_date_missing_sav(js, high) :  get_double_missing(js, high);
+        readstat_variable_add_missing_double_range(var, lo, hi);
+    } else if (low && high) {
+        fprintf(stderr, "%s:%d unsupported output format for missing range\n", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+    if (discrete && c->output_format == RS_FORMAT_SAV) {
+        double v = is_date ? get_double_date_missing_sav(js, discrete) : get_double_missing(js, discrete);
+        readstat_variable_add_missing_double_value(var, v);
+    } else if (discrete) {
+        fprintf(stderr, "%s:%d missing.discrete-value specified, but unsupported output format\n", __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void produce_missingness(struct csv_metadata *c, const char* column) {
+    const char *js = c->json_md->js;
+    readstat_variable_t* var = &c->variables[c->columns];
     var->missingness.missing_ranges_count = 0;
     
     jsmntok_t* missing = find_variable_property(js, c->json_md->tok, column, "missing");
@@ -185,29 +251,19 @@ void produce_missingness(struct csv_metadata *c, const char* column) {
         return;
     }
 
-    jsmntok_t* values = find_object_property(js, missing, "values");
-    if (!values) {
-        fprintf(stderr, "%s:%d Expected to find missing 'values' property\n", __FILE__, __LINE__);
-        return;
+    jsmntok_t* missing_type = find_object_property(js, missing, "type");
+    if (!missing_type) {
+        fprintf(stderr, "%s:%d expected to find missing.type for column %s\n", __FILE__, __LINE__, column);
+        exit(EXIT_FAILURE);
     }
 
-    int j = 1;
-    for (int i=0; i<values->size; i++) {
-        jsmntok_t* missing_value_token = values + j;
-        if (c->output_format == RS_FORMAT_SAV && var->type == READSTAT_TYPE_DOUBLE && is_date) {
-            readstat_variable_add_missing_double_value(var, get_double_date_missing_sav(js, missing_value_token, i));
-        } else if (c->output_format == RS_FORMAT_DTA && var->type == READSTAT_TYPE_INT32 && is_date) { 
-            // TODO I think this is correct, but would be good to verify
-            // So it seems that STATA does not use the hi-low ranges, and thus we only need to write once
-            readstat_value_t v = get_int32_date_missing_dta(js, missing_value_token, i);
-            var->missingness.missing_ranges[var->missingness.missing_ranges_count++] = v;
-        } else if (var->type == READSTAT_TYPE_DOUBLE) {
-            readstat_variable_add_missing_double_value(var, get_double_missing(js, missing_value_token, i));
-        } else {
-            fprintf(stderr, "%s:%d Unsupported column type %d\n", __FILE__, __LINE__, var->type);
-            exit(EXIT_FAILURE);
-        }
-        j += slurp_object(missing_value_token);
+    if (match_token(js, missing_type, "DISCRETE")) {
+        produce_missingness_discrete(c, missing, column);
+    } else if (match_token(js, missing_type, "RANGE")) {
+        produce_missingness_range(c, missing, column);
+    } else {
+        fprintf(stderr, "%s:%d unknown missing type %.*s\n", __FILE__, __LINE__, missing_type->end - missing_type->start, js+missing_type->start);
+        exit(EXIT_FAILURE);
     }
 }
 
