b3d44c08618d86ce9523193c95a16b0564e9d9a4
Ivar Refsdal <ivar.refsdal@nsd.no>
2017-03-29 13:15:18 +0200

    Use strict mode for jsmn

--- src/bin/modules/jsmn.c
+++ src/bin/modules/jsmn.c
@@ -1,5 +1,7 @@
 #include "jsmn.h"
 
+#define JSMN_STRICT 1
+
 /**
  * Allocates a fresh unused token from the token pull.
  */
@@ -198,6 +200,9 @@ int jsmn_parse(jsmn_parser *parser, const char *js, size_t len,
 						break;
 					}
 					if (token->parent == -1) {
+						if(token->type != type || parser->toksuper == -1) {
+							return JSMN_ERROR_INVAL;
+						}
 						break;
 					}
 					token = &tokens[token->parent];
